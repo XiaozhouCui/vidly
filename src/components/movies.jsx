9d2bccd905293685b037602b93ce7168257da01a
Joe Cui <joe.cui@bpanz.com>
2020-04-13 22:38:10 +1000

    Pagination added with PropTypes validation

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Like from "./like";
 import Pagination from "./pagination";
 import { getMovies } from "../services/fakeMovieService";
+import { paginate } from "../utils/paginate";
 
 class Movies extends Component {
   state = {
@@ -31,8 +32,12 @@ class Movies extends Component {
   render() {
     // save length of movies array as 'count'
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage } = this.state;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
+
     if (count === 0) return <p>There are no movies in the database.</p>;
+
+    const movies = paginate(allMovies, currentPage, pageSize);
+
     return (
       <React.Fragment>
         <p>Showing {count} movies in the database.</p>
@@ -48,7 +53,7 @@ class Movies extends Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.movies.map((movie) => (
+            {movies.map((movie) => (
               <tr key={movie._id}>
                 <td>{movie.title}</td>
                 <td>{movie.genre.name}</td>
