c21e578a58f7121c05f69cde1aeebcf3bb5bd55c
Joe Cui <joe.cui@bpanz.com>
2020-04-18 09:13:22 +1000

    Added form validation for login page

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Input = ({ name, label, value, onChange }) => {
+const Input = ({ name, label, value, error, onChange }) => {
   // ({ name, label, value, onChange }) are the destructured properties/methods of (props) object
   // Interface: { name, label, value, onChange } = props
   return (
@@ -14,6 +14,8 @@ const Input = ({ name, label, value, onChange }) => {
         type="text"
         className="form-control"
       />
+      {error && <div className="alert alert-danger">{error}</div>}
+      {/* if error is truthy, alert div will be returned; if error is falsy, alert div will be ignored */}
     </div>
   );
 };
--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,25 +1,61 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "./common/input";
 
 class LoginForm extends Component {
   state = {
     account: { username: "", password: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+
+  validate = () => {
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    if (!error) return null;
+
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
+
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value }; // [name] is a computed property
+    const schema = { [name]: this.schema[name] }; // save a sub-schema using computed properties
+    const { error } = Joi.validate(obj, schema); // Joi.validate() will return a result obj, sve result.error as "error"
+    return error ? error.details[0].message : null;
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
+
+    const errors = this.validate(); // validate() will return null if no error
+    this.setState({ errors: errors || {} }); // if errors is truthy, setState({ errors: errors}); if error is null/falsy, setState({ errors: {}});
+    if (errors) return; // don't call server if there is an error, return immediately.
+
+    // Call the server
     console.log("Submitted");
   };
 
-  handleChange = (e) => {
-    // (e) can be destructured to ({ currentTarget: input}), then replace all "e.currentTarget" with "input"
+  handleChange = ({ currentTarget: input }) => {
+    // ({ currentTarget: input}) is destructuring event object and save e.currentTarget as "input"
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
     const account = { ...this.state.account };
-    account[e.currentTarget.name] = e.currentTarget.value; // don't use dot notation (account.username), because bracket notation (account[e.currentTarget.name]) can access multiple properties dynamically
-    this.setState({ account });
+    account[input.name] = input.value; // don't use dot notation (account.username), because bracket notation (account[e.currentTarget.name]) can access multiple properties dynamically
+
+    this.setState({ account, errors });
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
 
     return (
       <div>
@@ -30,14 +66,18 @@ class LoginForm extends Component {
             value={account.username} // making it a controlled element
             label="Username"
             onChange={this.handleChange} // two-way binding
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn btn-primary">
+            Login
+          </button>
         </form>
       </div>
     );
